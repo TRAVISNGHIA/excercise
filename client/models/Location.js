671b570ad4780f3be8bb41a5f2233da2d9f82ace
TRAVISNGHIA <nguyenhuunghia20bn@gmail.com>
2025-04-03 09:46:56 +0700

    Delete client directory

--- client/models/Location.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const LocationSchema = new mongoose.Schema({
-    encodedId: String,
-    address: String,
-});
-
-export default mongoose.models.Location || mongoose.model("Location", LocationSchema);
--- client/models/UrlMatch.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const urlSchema = new mongoose.Schema({
-    url: { type: String, required: true },
-});
-
-const UrlMatch = mongoose.models.UrlMatch || mongoose.model('UrlMatch', urlSchema, 'urlmatchs');
-
-export default UrlMatch;
