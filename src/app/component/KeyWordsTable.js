c6b567ab492b04c12203c0a372c69f8b026a740f
nghia <nguyenhuunghia20bn@gmail.com>
2025-04-04 14:18:22 +0700

    a

--- src/app/component/KeyWordsTable.js
+++ src/app/component/KeyWordsTable.js
@@ -14,7 +14,7 @@ export default function KeywordsTable() {
     const [data, setData] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [editingData, setEditingData] = useState({});
-    const API_URL = "http://localhost:3000/api/keywords";
+    const API_URL = "https://excercise-duu9.onrender.com/api/keywords";
 
     useEffect(() => { fetchData(); }, []);
 
